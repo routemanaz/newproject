7f1f475bb4dd1c96da5db356924d638e0e95ba8a
Abdul Aziz <abubekrabdulaziz@gmail.com>
2023-03-09 11:04:05 +0000

    Files have been added

---
+++ resources/prac.js
@@ -0,0 +1,21 @@
+const square = function (x) {
+  return x * x;
+};
+
+console.log(square(12));
+
+const power = function (base, exponent) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+
+let min = function(15,20){
+    return Math.min
+    console.log(min)
+    
+  }
+
+console.log(min);
